5ddb2224974512d98737581d052c38fc5ed656df
Reid, Jon Carl <jreid2@hk223-27731.local>
2018-11-18 21:31:54 -0800

    Initial Commit

---
+++ SpacePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Reid, Jon Carl on 11/18/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
